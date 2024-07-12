8658b0227838cecda991d7d9cac49d158a12abbe
Mathieu Delsaut <mathieu.delsaut@univ-reunion.fr>
2024-07-12 16:01:13 +0400

    Print sur des matrices

--- Hello.py
+++ Hello.py
@@ -1,2 +1,11 @@
 
-print('Hello World')
+print('Hello World') 
+
+# Define a matrix
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+# Print the matrix
+for row in matrix:
+    for element in row:
+        print(element, end=' ')
+    print()
